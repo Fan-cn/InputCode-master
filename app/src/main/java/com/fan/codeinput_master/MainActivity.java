e5cd139267a4801c5eda345108265938e7187536
fan <blackeyesfan@gmail.com>
2018-04-28 16:51:41 +0800

    commit

--- app/src/main/java/com/fan/codeinput_master/MainActivity.java
+++ app/src/main/java/com/fan/codeinput_master/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.fan.corelibrary.InputCode;
 
@@ -14,10 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         InputCode editText =  findViewById(R.id.edit_text);
+        final TextView textView =  findViewById(R.id.text_view);
         editText.setListener(new InputCode.OnInputCompleteListener() {
             @Override
             public void onComplete(String code) {
                 Log.e("----", code);
+                textView.setText(code);
             }
         });
     }
